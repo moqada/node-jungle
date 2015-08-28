fff7ebc55d1f5f0990e315ca0085aac5bdbf8093
moqada <moqada@gmail.com>
2015-08-29 03:13:41 +0900

    feat(commands): Add --table option

--- src/commands/jungle-ec2.js
+++ src/commands/jungle-ec2.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import Table from 'cli-table';
 import debugModule from 'debug';
 import program from 'commander';
 import {defaultOutputHelp, errorAndExit, noCommandAndExit} from './utils';
@@ -7,6 +8,17 @@ import {getTagValue} from '../utils';
 
 const debug = debugModule('jungle:cli');
 
+const DEFAULT_TABLE_OPTIONS = {
+  chars: {
+    top: '', 'top-mid': '', 'top-left': '', 'top-right': '',
+    bottom: '', 'bottom-mid': '', 'bottom-left': ' ', 'bottom-right': ' ',
+    left: ' ', 'left-mid': '',
+    mid: '-', 'mid-mid': '+',
+    right: '', 'right-mid': '',
+    middle: '|'
+  },
+  style: {compact: true, head: ['gray']}
+};
 
 const STATUS_STYLES = {
   stopped: chalk.red,
@@ -26,19 +38,28 @@ program.action(noCommandAndExit('jungle ec2'));
 
 program.command('ls [name]')
 .description('List EC2 Instances')
+.option('-t, --table', 'Table style output')
 .action((name, options) => {
   const jungle = new Jungle(options.parent.region);
   jungle.ec2.getInstances(name).then(instances => {
-    instances.forEach(i => {
-      const cols = [
+    const rows = instances.map(i => {
+      return [
         getTagValue(i.Tags, 'Name') || '',
         coloredStatus(i.State.Name),
         i.InstanceId,
         i.PrivateIpAddress || '',
         i.PublicIpAddress || ''
       ];
-      console.log(cols.join('\t'));
     });
+    if (options.table) {
+      const table = new Table(Object.assign({
+        head: ['Name', 'State', 'InstanceId', 'PrivateIp', 'PublicIp']
+      }, DEFAULT_TABLE_OPTIONS));
+      table.push(...rows);
+      console.log(table.toString());
+    } else {
+      rows.forEach(row => console.log(row.join('\t')));
+    }
   }).catch(errorAndExit);
 });
 
