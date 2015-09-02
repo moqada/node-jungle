27314ef17d268fc08b00658f3641bf313834546e
moqada <moqada@gmail.com>
2015-09-02 20:53:16 +0900

    feat(inner): Add utils.filterObjects

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,25 @@
+import minimatch from 'minimatch';
+
+
 export function getTagValue(tags, key) {
   const tag = Array.find(tags, t => t.Key === key);
   return tag && tag.Value;
 }
 
-export default {getTagValue};
+
+export function filterObjects(configs, objects) {
+  return configs.reduce((results, config) => {
+    if (!config.query) {
+      return results;
+    }
+    return results.filter(o => {
+      return minimatch(o[config.key], config.query, {
+        nobrace: true,
+        noglobstar: true,
+        noext: true
+      });
+    });
+  }, objects);
+}
+
+export default {getTagValue, filterObjects};
