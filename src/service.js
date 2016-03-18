f2d9150fb1f1790fd31d7506a03b56c31b0e1a64
moqada <moqada@gmail.com>
2016-03-18 19:47:30 +0900

    docs(node-jungle): Add jsdoc

--- src/service.js
+++ src/service.js
@@ -1,7 +1,15 @@
 import AWS from 'aws-sdk';
 
 
+/**
+ * Service
+ */
 export default class Service {
+  /**
+   * Constructor
+   *
+   * @param {Object} [opts={}] - Options
+   */
   constructor(opts = {}) {
     const sdkName = this.constructor.SDKName || this.constructor.name;
     this._sdk = new AWS[sdkName](opts);
